3e309e82e1d516a1be64261bfa67a65c6f689bd5
WingBot <zerongsu@163.com>
2019-09-01 22:17:35 +0800

    add unitreepatent files

---
+++ projects/UnitreePatent/_data/16CEB3C8FECEMUND9JYEPDJEZLTKHPH0XWHQ/default_files/literature_and_annotations.mm
@@ -0,0 +1,12 @@
+<map version="docear 1.1" type="literature_annotations" dcr_id="1567314775045_1y2dj2wpep2h575qxq5xms6vk" project="16CEB3C8FECEMUND9JYEPDJEZLTKHPH0XWHQ" project_last_home="file:/home/zech/Docear/projects/UnitreePatent/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Literature &amp; Annotations" FOLDED="false" ID="ID_84652018" CREATED="1567314775030" MODIFIED="1567314775046" DCR_PRIVACY_LEVEL="DEMO">
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<attribute NAME="mon_incoming_folder" VALUE="@@literature_repository@@"/>
+<attribute NAME="mon_mindmap_folder" VALUE="@@library_mindmaps@@"/>
+<attribute NAME="mon_auto" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_subdirs" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_add_extra_incoming_node" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_flatten_dirs" VALUE="2" OBJECT="java.lang.Integer|2"/>
+</node>
+</map>
